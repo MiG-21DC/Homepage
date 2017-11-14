b044e136cd0ce14ac077f102e0e3c11e4019d235
Shawn <da_capo_usa@hotmail.com>
2017-11-14 08:36:12 +0800

    Hello World

--- script/main.py
+++ script/main.py
@@ -4,8 +4,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return "<h1 style='color:blue'>Hello World!</h1>"
+
+@app.route('/about')
+def about():
+    return '"<h1 style='color:blue'>This site is built with Nginx, uWSGI and Flask on an Amazon Ubuntu server with SSL encrypted</h1>"'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
